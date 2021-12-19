9c62d3764931cde311dc059ba8eb8cd6f0db9db8
Shaunak Guchhait <sguchhait0806@gmail.com>
2021-12-19 16:48:48 +0530

    added synonyms functionality

--- script.js
+++ script.js
@@ -1,15 +1,47 @@
 const wrapper = document.querySelector(".wrapper"),
+    synonym = document.querySelector(".synonyms .list "),
     searchInput = wrapper.querySelector("input"),
     infoText = wrapper.querySelector(".info-text");
 
 //data function 
 function data(result, word) {
     if (result.title) {
+        //message returned for not able to find the meaning
         infoText.innerHTML = `Cannot find the meaning of <span>"${word}"</span>. Please try to search for another word.`;
     } else {
         console.log(result);
         wrapper.classList.add("active");
+        let definition = result[0].meanings[0].definitions[0];
+        phonetics = `${result[0].meanings[0].partOfSpeech} /${result[0].phonetics[0].text}/`;
+
+
+        //passing data to their respective element
+        document.querySelector(".word p").innerHTML = result[0].word;
+        document.querySelector(".word span").innerHTML = phonetics;
+        document.querySelector(".meaning span").innerHTML = definition.definition;
+        document.querySelector(".example span").innerHTML = definition.example;
+        synonym.innerHTML = "";
+
+
+        if (definition.synonyms[0] == undefined) {
+            //if there is no synonym then hide the synonyms div
+            synonym.parentElement.style.display = "none";
+        } else {
+
+            synonym.parentElement.style.display = "block";
+            synonym.innerHTML = "";
+
+            //getting 5 synonyms and passing them inside synonyms div
+            for (let i = 0; i < 5; i++) {
+                let tag = `<span>${definition.synonyms[i]},</span>`;
+                synonym.insertAdjacentHTML("beforeend", tag);
+
+            }
+        }
+
     }
+
+
 }
 // fetch API function
 function fetchApi(word) {
